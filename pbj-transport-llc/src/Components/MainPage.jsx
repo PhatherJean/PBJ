6dfaf5936288dbbcf4b7a10df8258eb914d5f856
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-05-21 13:37:13 -0400

    Removed the randome text from page

--- pbj-transport-llc/src/Components/MainPage.jsx
+++ pbj-transport-llc/src/Components/MainPage.jsx
@@ -13,32 +13,6 @@ export default function MainPage(props) {
             <p>Still under construction we will be back up soon </p>
           </div>
         </div>
-        <section className="homepage">
-          <p>
-            Once the cab chassis decelerated the . The body dumped the
-            vocational motortruck but the shock-resistant engine was developed
-            by the Hino. Once the van drove the Detroit? The platform body
-            developed the aluminum ProMaster City. The steel, 2x4 mechanic
-            fixed! The Dodge demolished the 4WD HV when once the service body
-            accelerated the 4x4! The RAM upfitted the tough wheel but the 2x4
-            van was developed by the crane. The service utility van totaled the
-            vocational gasoline engine while the upfitted axle was fixed by the
-            gasoline engine! The combo body deconstructed the diesel vehicle.
-            The diesel F-750 was crashed by the stake bed. The galvanized,
-            diesel wheel throttled. The lifted cutaway van was trucked by the
-            contractor body. The cylinder developed the lifted hand truck. The
-            pickup manufactured the work-ready combo body. The diesel tire was
-            decelerated by the upfitted cargo van. The galvanized cargo was
-            accelerated by the refrigerated body. The ladder rack manufactured
-            the durable tire. The F-750 constructed the galvanized welder body
-            therefore the durable 4 Wheel Drive was drove by the automobile.
-            Once the cab chassis accelerated the cutaway while the 4x4 diesel
-            engine was dumped by the ute? The glass body developed the diesel
-            NRR? Once the cab to axle ratio upfitted the 4 Wheel Drive but the
-            ignition decelerated the 4WD tailboard? The steel garbage truck was
-            throttled by the vehicle.
-          </p>
-        </section>
       </div>
       <section className="bio">
         <div>
