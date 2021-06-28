c38596bf2a726fbfb3e7883a6d380626d8b9b746
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-06-28 17:15:44 -0400

    I got the home page done

--- pbj-transport-llc/src/Components/MainPage.jsx
+++ pbj-transport-llc/src/Components/MainPage.jsx
@@ -2,28 +2,6 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 export default function MainPage(props) {
-  // const text = ["websites", "illustration", "pancakes"];
-
-  // let count = 0;
-  // let index = 0;
-  // let currentText = "";
-  // let letter = "";
-
-  // (function typed() {
-  //   if (count === text.length) {
-  //     count = 0;
-  //   }
-  //   currentText = text[count];
-  //   letter = currentText.slice(0, ++index);
-
-  //   document.querySelector(".type").textContent = letter;
-  //   if (letter.length === currentText.length) {
-  //     count++;
-  //     index = 0;
-  //   }
-  //   setTimeout(typed, 400);
-  // })();
-
   return (
     <div className="homePage">
       <div className="homePageImg">
@@ -54,17 +32,27 @@ export default function MainPage(props) {
       <section className="bio">
         <div className="smallGuy">
           <h3 className="subtitle">Who is PBJ Transport?</h3>
-          <p className="smallGuy-info">
+          <p className="smallguy info">
             We are a small Florida trucking company built by professional truck
             drivers. With a combine experience of 5 years driving under our belt
             you can rest assured your load will get to its destination.
           </p>
         </div>
+        <br />
         <div className="time">
           <h3 className="subtitle">Time is Money</h3>
-          <p className="time-info">
+          <p className="times info">
             We are a firm believer in time is of the essence. Getting your load
             to it's destination in a safe manner is our #1 priority.
+          </p>
+        </div>
+        <br />
+        <div className="world">
+          <h4 className="subtitle">Service Area</h4>
+          <p className="usa info">
+            {" "}
+            Your customers is in our service area if they are in the continguous
+            48 states.{" "}
           </p>
         </div>
       </section>
